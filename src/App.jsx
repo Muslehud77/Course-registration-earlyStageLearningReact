8de49b53fca15c0878764eab36ac40d878eb1eae
Muslehud77 <fardinmohit@gmail.com>
2023-09-15 21:43:18 +0800

    added-modalAndToast-Notification

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import Cart from "./Components/Cart/Cart"
 import Cards from "./Components/cards/Cards"
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
  const [carts,setCarts] = useState([])
@@ -8,7 +10,19 @@ function App() {
   const [credit,setCredit] = useState(0)
   const [price,setPrice] = useState(0)
 
+const notify = () => {
 
+  toast.error("This Course is Already Added to Your Cart", {
+position: "bottom-center",
+autoClose: 5000,
+hideProgressBar: true,
+closeOnClick: true,
+pauseOnHover: true,
+draggable: true,
+progress: undefined,
+theme: "dark",
+});
+}
 
 
  const handleCart = (card) =>{
@@ -17,7 +31,7 @@ function App() {
   let credit = parseInt(card.credit.replace('hours','' ));
 
   if(isExist){
-   return alert('Already exist')
+   return notify()
   }else{
 
     carts.forEach(cart => {
@@ -38,7 +52,7 @@ function App() {
 
 
     }else{
-      alert('Your credit reached maximum')
+      document.getElementById('limit').showModal()
     }
    
   }
@@ -54,6 +68,30 @@ function App() {
         <Cards handleCart={handleCart}></Cards>
         <Cart price={price} creditRemaining={creditRemaining} credit={credit} carts={carts}></Cart>
       </div>
+
+{/* Limit Modal  */}
+      <div>
+              {/* Open the modal using document.getElementById('ID').showModal() method */}
+
+<dialog id="limit" className="modal">
+  <div className="modal-box">
+    <h3 className="font-bold text-lg">Reached Limit!</h3>
+    <p className="py-4 font-bold">You Don't Have Enough Credit Hours. <br />
+   <span className="font-normal text-red-500"> Your remaining credit is {creditRemaining} {creditRemaining<=1? 'Hour' : 'Hours'} </span></p>
+    
+    <div className="modal-action">
+      <form method="dialog">
+        {/* if there is a button in form, it will close the modal */}
+        <button className="btn">Close</button>
+      </form>
+    </div>
+  </div>
+</dialog>
+      </div>
+
+
+<ToastContainer />
+
     </>
   )
 }
